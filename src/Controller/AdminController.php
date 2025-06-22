133bb4b7ff90f6f37160adff117140d5614a1e49
aradhanaas <aradhanashaw128@gmail.com>
2025-06-22 23:33:11 +0800

    base admin UI

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Admin;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AdminController extends AbstractController
+{
+    // route to admin page for development!
+    #[Route('/admin', name: 'admin_page')]
+    public function admin(): Response
+    {
+        return $this->render('admin/admin.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
+    #[Route('/admin/manage_events', name: 'manage_events_page')]
+    public function manage_events(): Response
+    {
+        return $this->render('admin/manage_events.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
+    #[Route('/admin/manage_users', name: 'manage_users_page')]
+    public function manage_users(): Response
+    {
+        return $this->render('admin/manage_users.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+}
