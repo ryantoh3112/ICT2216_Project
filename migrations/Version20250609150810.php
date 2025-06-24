8cd96df9e0c94569a606ed4519646b24c00e3d4a
yhdeon <deonprojdev@hotmail.com>
2025-06-25 02:25:31 +0800

    DB live phase 5

--- migrations/Version20250609150810.php
+++ migrations/Version20250609150810.php
@@ -12,6 +12,11 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20250609150810 extends AbstractMigration
 {
+    public function isTransactional(): bool
+    {
+        return false;
+    }
+
     public function getDescription(): string
     {
         return '';
